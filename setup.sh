8f23a904950663ad6a9d8adf4ab7a9a2ce5956a7
przemeko <pkocznur@gmail.com>
2020-01-16 23:02:21 +0100

    add vim-fugitive plugin

--- setup.sh
+++ setup.sh
@@ -2,27 +2,40 @@
 
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
 echo $'\nInstalling pathogen'
-    mkdir -p ~/.vim/autoload ~/.vim/bundle
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+   mkdir -p ~/.vim/autoload ~/.vim/bundle
+   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
 if [ ! -d ~/.vim/pack/themes/opt/solarized8 ]; then
 echo $'\nInstalling solarized theme'
-git clone https://github.com/lifepillar/vim-solarized8.git \
+  git clone https://github.com/lifepillar/vim-solarized8.git \
     ~/.vim/pack/themes/opt/solarized8
 fi
 
-# airline
 if [ ! -d ~/.vim/bundle/vim-airline ]; then
 echo $'\nInstalling vim-airline plugin'
   git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 fi
 
-echo $'\nSet .vimrc file'
+if [ ! -d ~/.vim/bundle/vim-fugitive ]; then
+  echo $'\nInstalling vim-fugitive plugin'
+  cd ~/.vim/bundle
+  git clone https://github.com/tpope/vim-fugitive
+fi
+
+if [ -f ~/.vimrc ]; then
+echo $'\nBackup .vimrc to .vimrc_copy'
+  cp ~/.vimrc ~/.vimrc_copy
+fi
+
+echo $'\nInit .vimrc file'
 cat > ~/.vimrc <<EOF
 
 set background=dark
 colorscheme solarized8_high
 syntax on
 set number
+let g:airline_powerline_fonts = 1
 execute pathogen#infect()
+
+EOF
